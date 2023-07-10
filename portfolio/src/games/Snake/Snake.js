fbab4fe6509a7aebb4deb5f2e6cb82adea915f79
NakUnagi <bojarski.wojciech026@gmail.com>
2023-07-10 22:49:35 +0200

    test2

--- portfolio/src/games/Snake/Snake.js
+++ portfolio/src/games/Snake/Snake.js
@@ -1,26 +1,70 @@
-import React, { useState, createRef, useEffect } from "react";
+import React, { createRef, Component } from "react";
 
-import './Snake.scss'
-const Snake = () => {
+import './Snake.scss'    
 
-    const snakeCanvas = createRef()
-    useEffect(() => {
-        document.body.style.setProperty('background', '#212529');
-    }, [])
+class Snake extends Component {
 
-    return (
-        <div className="snake-canvas-container">
-            <canvas
-                ref = {snakeCanvas}
-                id='canvSnake'
-                width='700'
-                height='700'
-            >
+    state = {
+        snake: [],
+        snakeLength: 5,
+        wallSize: 10
+    }
 
-            </canvas>
-        </div>
-    )
+    snakeCanvas = createRef()
+
+    render() {
+        return (
+            <div className="snake-canvas-container">
+                <canvas
+                    ref = {this.snakeCanvas}
+                    id='canvSnake'
+                    width='1200'
+                    height='600'
+                >
     
+                </canvas>
+            </div>
+        )
+    }
+
+    clearCanvas = () => {
+        const context2D = this.snakeCanvas.current.getContext('2d')
+        const canvaW = this.snakeCanvas.current.width
+        const canvaH = this.snakeCanvas.current.height
+        context2D.fillStyle = '#000000'
+        context2D.fillRect(0, 0, canvaW, canvaH)
+    }
+
+    makeSnake = () => {
+        console.log('początek')
+        let canvaX = this.snakeCanvas.current.width
+        let canvaH = this.snakeCanvas.current.height
+        for(let i=0; i < this.state.snakeLength; i++){
+            let x = canvaX/2 + i*this.state.wallSize
+            let y = canvaH/2
+            this.setState((prev) => ({
+                snake: [...prev.snake, {x:x, y:y} ]
+            }))
+        }
+    }
+
+    StartGame = () => {
+        this.makeSnake()
+
+        setInterval(() => {
+            this.clearCanvas()
+        }, 1000/60)
+    }
+
+    componentDidMount() {
+        document.body.style.setProperty('background', '#212529');
+        this.StartGame()
+    }
+
+    componentDidUpdate(){
+        console.log(this.state.snake)
+
+    }
 }
 
 export default Snake
